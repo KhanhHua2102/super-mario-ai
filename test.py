a061b3f678ddb3d95a1fb0df83b03a981cbe7222
Quang Khanh Hua (Henry) <henry@khanhhua2102.com>
2023-08-16 18:23:27 +0800

    move right

--- test.py
+++ test.py
@@ -3,19 +3,28 @@ import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 import gym
 
+from gym.wrappers.gray_scale_observation import GrayScaleObservation
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+
+JoypadSpace.reset = lambda self, **kwargs: self.env.reset(**kwargs)
+
+# Create the base environment
 env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode="human")
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
+env = GrayScaleObservation(env, keep_dim=True)
+# Wrap env in a DummyVecEnv
+env = DummyVecEnv([lambda: env])
+# Stack the frames
+env = VecFrameStack(env, n_stack=4)
 
 done = True
+
 env.reset()
-for step in range(5000):
-    action = env.action_space.sample()
-    obs, reward, terminated, truncated, info = env.step(action)
-    done = terminated or truncated
+
+for _idx in range(1000):
+    obs, reward, done, info = env.step([1])
 
     if done:
         state = env.reset()
     
 env.close()
-
-print("hello world")
