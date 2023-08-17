cbefda2fbc6e51293de1f5cbf9892cc9080fbeaa
Quang Khanh Hua (Henry) <henry@khanhhua2102.com>
2023-08-17 23:59:22 +0800

    initial q_learning and define states

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ https://python-poetry.org/docs/master/
 How to use poetry python package manager: 
 https://medium.com/analytics-vidhya/poetry-finally-an-all-in-one-tool-to-manage-python-packages-3c4d2538e828
 
+Q_learning repo for reference:
+https://github.com/simoninithomas/Deep_reinforcement_learning_Course/blob/master/Q%20learning/FrozenLake/Q%20Learning%20with%20FrozenLake.ipynb
+
---
+++ states.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+
+# 1: small pipe
+# 2: medium pipe
+# 3: large pipe
+# 4: near small pipe
+# 5: near medium pipe
+# 6: near large pipe
+# 7: 1 enemy
+# 8: 2 enemies
+# 9: 1 and 2 enemies
+# 10: 2 and 1 enemies
+# 11: 3 enemies
+# 12: hole
+
+
+def get_state(obs):
+    small_pipe, medium_pipe, large_pipe, near_small_pipe, near_medium_pipe, near_large_pipe, one_enemy, two_enemies, one_and_two_enemies, two_and_one_enemies, three_enemies, hole = False, False, False, False, False, False, False, False, False, False, False, False
+    
+    if small_pipe:
+        return 0
+    elif medium_pipe:
+        return 1
+    elif large_pipe:
+        return 2
+    elif near_small_pipe:
+        return 3
+    elif near_medium_pipe:
+        return 4
+    elif near_large_pipe:
+        return 5
+    elif one_enemy:
+        return 6
+    elif two_enemies:
+        return 7
+    elif one_and_two_enemies:
+        return 8
+    elif two_and_one_enemies:
+        return 9
+    elif three_enemies:
+        return 10
+    elif hole:
+        return 11
+    else:
+        return 12
