64bc1ed4483e2cf35d6ff3848df531a4295ceba2
KhanhHua2102 <henry@khanhhua2102.com>
2023-09-21 21:33:13 +0800

    enhance using obs and action tuble for q_table dict

--- q_learning_obs.py
+++ q_learning_obs.py
@@ -1,3 +1,4 @@
+import json
 import random
 import time
 import warnings
@@ -5,11 +6,17 @@ import warnings
 import gym
 import gym_super_mario_bros
 from gym.wrappers.gray_scale_observation import GrayScaleObservation
+from matplotlib import pyplot as plt
 import numpy as np
 from nes_py.wrappers import JoypadSpace
 
-from mario_actions import CUSTOM_MOVEMENT_3
-from states import get_state
+
+CUSTOM_MOVEMENT = [
+    ["NOOP"],
+    ["right"],
+    ["right", "A"],
+    ["A"],
+]
 
 # Suppress all warnings (not recommended for production code)
 warnings.filterwarnings("ignore")
@@ -17,36 +24,63 @@ warnings.filterwarnings("ignore")
 JoypadSpace.reset = lambda self, **kwargs: self.env.reset(**kwargs)
 # Create the base environment
 env = gym.make(
-    "SuperMarioBros-1-1-v0", apply_api_compatibility=True, render_mode="human"
+    "SuperMarioBros-1-1-v0", apply_api_compatibility=True, render_mode="rgb_array"
 )
 env = GrayScaleObservation(env, keep_dim=False)
-env = JoypadSpace(env, CUSTOM_MOVEMENT_3)
+env = JoypadSpace(env, CUSTOM_MOVEMENT)
 
 
-action_size = 4
-state_size = 240*256
+DELAY = 0.0
+ACTION_SIZE = 4
 
-q_table = np.zeros((state_size, action_size))
-print(q_table.shape)
-
-delay = 0.0
-
-total_episodes = 5000  # Total episodes
-learning_rate = 0.8  # Learning rate
-max_steps = 1000  # Max steps per episode
-gamma = 0.95  # Discounting rate
+TOTAL_EPISODES = 50  # Total episodes
+LEARNING_RATE = 0.8  # Learning rate
+MAX_STEPS = 1000  # Max steps per episode
+GAMMA = 0.95  # Discounting rate
 
 # Exploration parameters
-epsilon = 1.0  # Exploration rate
-max_epsilon = 1.0  # Exploration probability at start
-min_epsilon = 0.01  # Minimum exploration probability
-decay_rate = 0.005  # Exponential decay rate for exploration prob
+EPSILON = 1.0  # Exploration rate
+MAX_EPSILON = 1.0  # Exploration probability at start
+MIN_EPSILON = 0.01  # Minimum exploration probability
+DECAY_RATE = 0.005  # Exponential decay rate for exploration prob
 
 # List of rewards
 rewards = []
 
-for episode in range(total_episodes):
-    print("Episode: " + str(episode))
+q_table = {}
+
+
+def getMaxAction(input_state, input_q_table) -> int:
+    max_action = -1
+    if (len(input_q_table) == 0):
+        return -1
+    for i in range(ACTION_SIZE):
+        curr_state = (str(input_state) + str(i))
+        if (curr_state not in input_q_table.keys()):
+            continue
+        if (input_q_table.get(curr_state) > max_action):
+            max_action = i
+        
+    return max_action
+
+def getMaxValue(input_state, input_q_table) -> float:
+    max_value = 0
+    if (len(input_q_table) == 0):
+        return -1
+    for i in range(ACTION_SIZE):
+        curr_state = (str(input_state), str(i))
+        if (curr_state not in input_q_table.keys()):
+            continue
+        if (input_q_table.get(curr_state) > max_value):
+            max_value = input_q_table.get(curr_state)
+
+        if (max_value == 0):
+            raise Exception("Max value is 0")
+    return max_value
+
+
+for episode in range(TOTAL_EPISODES):
+    print("\nEpisode: " + str(episode + 1))
     env.reset()
     obs, reward, terminated, truncated, info = env.step(0)
 
@@ -54,46 +88,60 @@ for episode in range(total_episodes):
     done = False
     total_rewards = 0
 
-    for step in range(max_steps):
+    for step in range(MAX_STEPS):
         exploration_exploitation_tradeoff = random.uniform(0, 1)
 
-        if exploration_exploitation_tradeoff > epsilon:
-            action = np.argmax(q_table[obs, :])
+        if exploration_exploitation_tradeoff > EPSILON and getMaxAction(obs, q_table) != -1:
+            action = getMaxAction(obs, q_table)
         else:
-            # reduce the likelihood of choosing jumping actions
-            if random.uniform(0, 1) > 0.5:
-                action = random.randint(0, 2)
-            else:
-                action = random.randint(3, 4)
-    
-        print("Action: " + str(action))
-        
-        time.sleep(delay)
+            action = random.randint(0, 3)
 
         obs, reward, terminated, truncated, info = env.step(action)
-        new_state = obs
-        
-        time.sleep(delay)
-        
-        print("Reward: " + str(reward))
+        obs = obs[obs.shape[0] // 2 : obs.shape[0] - 15, :]
+        time.sleep(DELAY)
         
         done = terminated or truncated
 
-        # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
-        q_table[obs, action] = q_table[obs, action] * (
-            1 - learning_rate
-        ) + learning_rate * (reward + gamma * np.max(q_table[new_state, :]))
-        
-        # print the q table in nice format
-        print(q_table)
+        # Update Q(s,a)= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
+        newState = (str(obs) + str(action))
+        if (newState not in q_table.keys() or len(q_table) == 0):
+            q_table[newState] = LEARNING_RATE * reward * getMaxValue(obs, q_table)
+        else:
+            q_table[newState] = float(q_table.get(newState)) * (1 - LEARNING_RATE) + LEARNING_RATE * (reward + GAMMA * getMaxValue(obs, q_table))
 
         total_rewards += reward
-        state = new_state
+        state = newState
 
-        if done == True:
+        if done:
             break
 
-    epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
+    EPSILON = MIN_EPSILON + (MAX_EPSILON - MIN_EPSILON) * np.exp(-DECAY_RATE * episode)
     rewards.append(total_rewards)
-print("Score over time: " + str(sum(rewards) / total_episodes))
-print(q_table)
+
+print("Score over time: " + str(sum(rewards) / TOTAL_EPISODES))
+
+with open("q_table.json", "w", encoding="utf-8") as json_file:
+    json.dump(q_table, json_file)
+
+
+action_list = []
+
+env.reset()
+obs, reward, terminated, truncated, info = env.step(0)
+obs = obs[obs.shape[0] // 2 : obs.shape[0] - 15, :]
+
+for move in range(100):
+    action = getMaxAction(obs, q_table)
+    if (action == -1):
+        action = random.randint(0, 3)
+    obs, reward, terminated, truncated, info = env.step(action)
+    obs = obs[obs.shape[0] // 2 : obs.shape[0] - 15, :]
+    action_list.append(action)
+
+env.close()
+
+print(action_list)
+
+with open("action_list.txt", "w", encoding="utf-8") as f:
+    for action in action_list:
+        f.write(str(action) + "\n")
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import hashlib
 import numpy as np
 import cv2
 
@@ -5,13 +6,40 @@ import cv2
 def stack_frames(frames, new_frame, stack_size):
     if frames is None:
         frames = np.zeros((stack_size, *new_frame.shape), dtype=np.uint8)
-    
+
     frames[:-1] = frames[1:]
     frames[-1] = new_frame
-    
+
     return frames
 
 
 def convert_to_grayscale(image):
     grayscale_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     return grayscale_image
+
+
+def hashState(obs, action) -> int:
+    # Convert the 3D array to a string representation
+    obs_str = str(obs)
+    action_str = str(action)
+    arr_str = obs_str + action_str
+
+    # Use a hash function to generate a unique hash for the array
+    hash_obj = hashlib.md5(arr_str.encode())
+
+    # Convert the hash to an integer (you can choose a different method if needed)
+    hash_int = int(hash_obj.hexdigest(), 16)
+
+    return hash_int
+
+
+
+# my_3d_array = [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]]
+# hash_value = hash3DArray(my_3d_array)
+# print(hash_value)
+
+# my_dict = {}
+
+# my_dict[(hash_value, 2)] = 512341
+
+# print(my_dict[(hash_value, 2)])
